e2fb00107d5fd5423f4bc0295e19cd3ee73c2154
Minh <trandinhquangminh98@gmail.com>
2019-11-15 11:47:31 +0700

    giaodien1

--- app/src/main/java/com/example/admin/fitnessyoga/Menu.java
+++ app/src/main/java/com/example/admin/fitnessyoga/Menu.java
@@ -1,7 +1,9 @@
 package com.example.admin.fitnessyoga;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Menu extends AppCompatActivity {
 
@@ -9,5 +11,33 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void btnGym(View view)
+    {
+        Intent intent = new Intent(Menu.this, Gym.class);
+        startActivity(intent);
+    }
+    public void btnYoga(View view)
+    {
+        Intent intent = new Intent(Menu.this, Yoga.class);
+        startActivity(intent);
+    }
+    //Yoga
+    public void btnCamera(View view)
+    {
+        Intent intent = new Intent(Menu.this, Camera.class);
+        startActivity(intent);
+    }
+    public void btnNote(View view)
+    {
+        Intent intent = new Intent(Menu.this, Gym.class);
+        startActivity(intent);
+    }
+
+    public void btnYogaa(View view)
+    {
+        Intent intent = new Intent(Menu.this, Yoga.class);
+        startActivity(intent);
     }
 }
